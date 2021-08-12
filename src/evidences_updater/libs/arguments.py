1926ee964d81baf66db252e000cd7cc3faac22c2
Phill Betancourt <phill.betan@gmail.com>
2021-08-12 16:27:01 -0500

    UPDATE: Evidences Transform Update and Upload Procces.
        The program for updating and uploading the new Evidence data was developed.
        In order to load the new Eidences, separate use is made of the data-release-tools / src / create_identifiers module and data-release-tools / src / replace_identifiers so that they can be valid at the time of loading.
    
        Phill Betancourt ☕

---
+++ src/evidences_updater/libs/arguments.py
@@ -0,0 +1,56 @@
+import argparse
+
+
+def load():
+    parser = argparse.ArgumentParser(description="Data updater/uploader, it will take the _id field as the field to get the object to update or to upload new evidences",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        "-d", "--directory",
+        help="Directory where the json files are located",
+        default="Results/"
+    )
+
+    parser.add_argument(
+        "-db", "--database",
+        help="Directory with the evidences to update",
+        default="regulondbmultigenomic",
+        metavar="regulondbmultigenomic"
+    )
+
+    parser.add_argument(
+        "-i", "--input-file",
+        help="JSON input file to process",
+        required=False
+    )
+
+    parser.add_argument(
+        "-l", "--log",
+        help="Directory that contains log of the invalid data, the reason why the data is being rejected.",
+        default="Results/log/"
+    )
+
+    parser.add_argument(
+        "-u", "--url",
+        help="URL to establish a connection between the process and MongoDB",
+        default="mongodb://localhost:27017/",
+        metavar="mongodb://localhost:27017/"
+    )
+
+    parser.add_argument(
+        "-upd",
+        "--update",
+        help="Sets the program to update existing evidences",
+        default="Results/update_evidences.json"
+    )
+
+    parser.add_argument(
+        "-upl",
+        "--upload",
+        help="Sets the program to upload new evidences",
+        default="Results/new_evidences.json"
+    )
+
+    arguments = parser.parse_args()
+
+    return arguments
